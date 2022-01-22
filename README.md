499fc63715a55b99a340da40a8f8ae7a60547ba3
VinceNguyen <nguyenquangv95@gmail.com>
2022-01-22 10:20:04 +0700

    Introduce-to-modal-2

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # React-Modal-Component
+
 A-resuable-modal-component
+
+## Modal-1
+
+Event-bubbling to detect click outside of the modal
+
+## Modal-2
+
+Use-ref to detect click outside of the modal
--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import Modal from "./components/Modal.tsx";
-// import "App.css";
+import "./App.css";
 
 const App = () => {
   const [isOpen, setIsOpen] = useState(false);
